4202397074ead47f036c17ae2b3d3ee64ac0724d
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-09-02 22:13:42 -0300

    Create near_hundred.py
    Warmup-1 > near_hundred
    prev  |  next  |  chance
    Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
    
    
    near_hundred(93) → True
    near_hundred(90) → True
    near_hundred(89) → False

---
+++ near_hundred.py
@@ -0,0 +1,48 @@
+"""
+Warmup-1 > near_hundred
+prev  |  next  |  chance
+Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+
+near_hundred(93) → True
+near_hundred(90) → True
+near_hundred(89) → False
+"""
+
+def near_hundred(n):
+  if n in range (90, 111):
+    return True
+  elif n in range (190, 211):
+    return True
+  else:
+    return False
+  
+  """
+  Expected	Run		
+near_hundred(93) → True	True	OK	
+near_hundred(90) → True	True	OK	
+near_hundred(89) → False	False	OK	
+near_hundred(110) → True	True	OK	
+near_hundred(111) → False	False	OK	
+near_hundred(121) → False	False	OK	
+near_hundred(-101) → False	False	OK	
+near_hundred(-209) → False	False	OK	
+near_hundred(190) → True	True	OK	
+near_hundred(209) → True	True	OK	
+near_hundred(0) → False	False	OK	
+near_hundred(5) → False	False	OK	
+near_hundred(-50) → False	False	OK	
+near_hundred(191) → True	True	OK	
+near_hundred(189) → False	False	OK	
+near_hundred(200) → True	True	OK	
+near_hundred(210) → True	True	OK	
+near_hundred(211) → False	False	OK	
+near_hundred(290) → False	False	OK	
+
+All Correct
+
+Solution:
+def near_hundred(n):
+  return ((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
+  
+"""
