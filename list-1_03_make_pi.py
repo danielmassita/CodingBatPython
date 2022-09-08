1dabb07171fae540b8f39a37c90ae3523d0bc11b
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-09-07 21:21:27 -0300

    Create list-1_03_make_pi.py
    List-1 > make_pi
    prev  |  next  |  chance
    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
    
    
    make_pi() → [3, 1, 4]

---
+++ list-1_03_make_pi.py
@@ -0,0 +1,19 @@
+"""
+List-1 > make_pi
+prev  |  next  |  chance
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+make_pi() → [3, 1, 4]
+"""
+
+def make_pi():
+  pi = [3, 1, 4]
+  return pi
+
+"""
+  Expected	Run		
+make_pi() → [3, 1, 4]	[3, 1, 4]	OK	
+
+All Correct
+"""
